5e42f3b5979d89e51a8991f7f1f598c99ec72a7c
Rohit Kumar Hansdah <rk8185109@gmail.com>
2023-05-08 11:55:36 +0530

    initial commit

---
+++ photoapp/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from taggit.managers import TaggableManager
+
+# Create your models here.
+
+class Photo(models.Model):
+
+    title = models.CharField(max_length=45)
+    description = models.CharField(max_length=250) 
+    created = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='photos/')
+    submitter = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    tags = TaggableManager() 
+
+    def __str__(self):
+        return self.title
+
+class Like(models.Model):
+
+    submitter = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='photos/')
+
+    def __str__(self):
+        return self.image
+    
+class Follow(models.Model):
+
+    submitter = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.submitter
+    
